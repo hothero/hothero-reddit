34243d55d03b46eebead9132fc0b00a6ecc15d5e
Ronald Hsu <hothero0705@gmail.com>
2017-06-09 00:52:02 +0800

    refactor posts schema

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,15 +1,16 @@
 module Api::V1
   class PostsController < ApplicationController
     # Generic API stuff here
-    $posts = []
+    $posts = {} # hash keys for indexing
     def index
-      render json: $posts
+      render json: $posts.values
     end
 
     def create
       # TODO: refactor the passing params
       # slice: fetch only valid params
-      $posts << Post.new(id: $posts.size + 1, title: post_params[:title], summary: post_params[:summary], url: post_params[:url])
+      id = $posts.size + 1
+      $posts[id.to_s] = Post.new(id: id, title: post_params[:title], summary: post_params[:summary], url: post_params[:url])
     end
 
     private
